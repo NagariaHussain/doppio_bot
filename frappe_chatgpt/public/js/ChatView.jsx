3ecbcabceaa5e79632827bffb4df08af5fb4ec09
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-04-09 16:21:50 +0530

    feat: chat messages UI

--- frappe_chatgpt/public/js/ChatView.jsx
+++ frappe_chatgpt/public/js/ChatView.jsx
@@ -1,13 +1,15 @@
 import {
   Flex,
   IconButton,
-  Input,
+  VStack,
   Box,
   Card,
   CardBody,
   Avatar,
   useToast,
   Textarea,
+  Text,
+  SkeletonText,
 } from "@chakra-ui/react";
 import { SendIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
@@ -20,17 +22,35 @@ const ChatView = () => {
   const toast = useToast();
   const [promptMessage, setPromptMessage] = useState("");
 
+  const [messages, setMessages] = useState([
+    {
+      from: "bot",
+      isLoading: false,
+      content: "How can I help you?",
+    },
+    {
+      from: "user",
+      isLoading: false,
+      content: "What do folks at Frappe do?",
+    },
+    {
+      from: "bot",
+      isLoading: true,
+      content: "",
+    },
+  ]);
+
   const handleSendMessage = () => {
-    if (!inputMessage.trim().length) {
+    if (!promptMessage.trim().length) {
       return;
     }
-    const data = inputMessage;
+    const data = promptMessage;
 
-    setMessages((old) => [...old, { from: "me", text: data }]);
-    setInputMessage("");
+    setMessages((old) => [...old, { from: "user", content: data }]);
+    setPromptMessage("");
 
     setTimeout(() => {
-      setMessages((old) => [...old, { from: "computer", text: data }]);
+      setMessages((old) => [...old, { from: "bot", content: data }]);
     }, 1000);
   };
 
@@ -49,10 +69,44 @@ const ChatView = () => {
       <Box
         width={"100%"}
         height={"100%"}
-        shadow={"lg"}
+        overflowY="scroll"
+        shadow={"xl"}
         rounded={"md"}
         backgroundColor={"white"}
-      ></Box>
+      >
+        <VStack spacing={2} align="stretch" p={"2"}>
+          {messages.map((message) => {
+            const fromBot = message.from === "bot";
+            return (
+              <Flex
+                direction={"column"}
+                p={"4"}
+                key={message.content}
+                alignSelf={fromBot ? "start" : "end"}
+                justify={"center"}
+                backgroundColor={fromBot ? "blackAlpha.800" : "linkedin.500"}
+                rounded={"xl"}
+                width={"fit-content"}
+                roundedTopLeft={fromBot ? "0" : "xl"}
+                roundedTopRight={fromBot ? "xl" : "0"}
+              >
+                {!message.isLoading ? (
+                  <Text color={"white"} mb={"0"}>
+                    {message.content}
+                  </Text>
+                ) : (
+                  <SkeletonText
+                    width="20"
+                    noOfLines={1}
+                    spacing="4"
+                    skeletonHeight="2"
+                  />
+                )}
+              </Flex>
+            );
+          })}
+        </VStack>
+      </Box>
 
       {/* Prompt Area */}
       <Card mt={"1.5"}>
@@ -66,6 +120,7 @@ const ChatView = () => {
               onKeyDown={(event) => {
                 if (event.code == "Enter" && event.metaKey && promptMessage) {
                   console.log("enter key pressed with message", promptMessage);
+                  handleSendMessage();
                   setPromptMessage("");
                 }
               }}
