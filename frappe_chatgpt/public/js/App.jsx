509de22fb9aa9a1671cd15e90c8ee98d5cb110b5
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-04-11 07:11:40 +0530

    fix: warning due to filename case

--- frappe_chatgpt/public/js/App.jsx
+++ frappe_chatgpt/public/js/App.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import ChatView from "./Chatview";
 import { nanoid } from 'nanoid'
+
+import ChatView from "./ChatView";
 
 export function App() {
 	// Unique sessionID for chat memory/history
